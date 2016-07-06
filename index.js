2295fdef048fb448df7fea9fd83f8c8ba1cb40be
Thomas Tuts <thomas@thomastuts.com>
2016-07-06 17:20:20 +0200

    Add solution code for both exercises (#4)
    * Add content and tests for exercises
    
    * Add solution code for both exercises
    
    * Remove explicit reference to README
    
    * Use spaces like an educated person

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+  
+  return {
+    addBump,
+    getBumps,
+  };
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
