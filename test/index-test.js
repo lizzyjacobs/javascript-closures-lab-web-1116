e189c6c7503cfc44955b56aca6f46488b2efb700
Charles Pletcher <cpletcher2@gmail.com>
2016-07-26 17:25:09 -0400

    Update tests

--- test/index-test.js
+++ test/index-test.js
@@ -1,56 +1,57 @@
+describe('closures', function() {
+  describe('Three point turn', function () {
+    it('should have a `bumpCounter()` function', () => {
+      expect(bumpCounter).toBeA('function');
+    });
 
-describe('Three point turn', function () {
-  it('should have a `bumpCounter()` function', () => {
-    expect(bumpCounter).toBeA('function');
+    it('should contain the other variables inside the closure', () => {
+      expect(window.addBump).toNotExist();
+      expect(window.getBumps).toNotExist();
+      expect(window.counter).toNotExist();
+    });
+
+    it('should return the `addBump()` and `getBumps()` function', () => {
+      const { addBump, getBumps } = bumpCounter();
+      expect(addBump).toBeA('function');
+      expect(getBumps).toBeA('function');
+    });
+
+    it('should return the counter when `getBumps()` is called', () => {
+      const { getBumps } = bumpCounter();
+      expect(getBumps()).toBeA('number');
+    });
+
+    it('should increase the counter when `addBump()` is called', () => {
+      const { addBump, getBumps } = bumpCounter();
+      expect(getBumps()).toEqual(0);
+      addBump();
+      expect(getBumps()).toEqual(1);
+    });
   });
 
-  it('should contain the other variables inside the closure', () => {
-    expect(window.addBump).toNotExist();
-    expect(window.getBumps).toNotExist();
-    expect(window.counter).toNotExist();
-  });
+  describe('Dangerous animals', function () {
+    it('should have a `createAnimal()` function', () => {
+      expect(createAnimal).toBeA('function');
+    });
 
-  it('should return the `addBump()` and `getBumps()` function', () => {
-    const { addBump, getBumps } = bumpCounter();
-    expect(addBump).toBeA('function');
-    expect(getBumps).toBeA('function');
-  });
+    it('should return a function when calling the `createAnimal()` closure', () => {
+      expect(createAnimal()).toBeA('function');
+    });
 
-  it('should return the counter when `getBumps()` is called', () => {
-    const { getBumps } = bumpCounter();
-    expect(getBumps()).toBeA('number');
-  });
+    it('should have a `sharkCreator` variable', () => {
+      expect(sharkCreator).toBeA('function');
+    });
 
-  it('should increase the counter when `addBump()` is called', () => {
-    const { addBump, getBumps } = bumpCounter();
-    expect(getBumps()).toEqual(0);
-    addBump();
-    expect(getBumps()).toEqual(1);
-  });
-});
+    it('should have a `sharkWithFrickinLaserbeam` variable', () => {
+      expect(sharkWithFrickinLaserbeam).toBeA('object');
+      expect(sharkWithFrickinLaserbeam.animalType).toEqual('Shark');
+      expect(sharkWithFrickinLaserbeam.deadlyDevice).toEqual('Laserbeam');
+    });
 
-describe('Dangerous animals', function () {
-  it('should have a `createAnimal()` function', () => {
-    expect(createAnimal).toBeA('function');
+    it('should have a `sharkWithFrickinCannon` variable', () => {
+      expect(sharkWithFrickinCannon).toBeA('object');
+      expect(sharkWithFrickinCannon.animalType).toEqual('Shark');
+      expect(sharkWithFrickinCannon.deadlyDevice).toEqual('Cannon');
+    });
   });
-
-  it('should return a function when calling the `createAnimal()` closure', () => {
-    expect(createAnimal()).toBeA('function');
-  });
-
-  it('should have a `sharkCreator` variable', () => {
-    expect(sharkCreator).toBeA('function');
-  });
-
-  it('should have a `sharkWithFrickinLaserbeam` variable', () => {
-    expect(sharkWithFrickinLaserbeam).toBeA('object');
-    expect(sharkWithFrickinLaserbeam.animalType).toEqual('Shark');
-    expect(sharkWithFrickinLaserbeam.deadlyDevice).toEqual('Laserbeam');
-  });
-
-  it('should have a `sharkWithFrickinCannon` variable', () => {
-    expect(sharkWithFrickinCannon).toBeA('object');
-    expect(sharkWithFrickinCannon.animalType).toEqual('Shark');
-    expect(sharkWithFrickinCannon.deadlyDevice).toEqual('Cannon');
-  });
-});
+})
--- test/root.js
+++ test/root.js
@@ -4,15 +4,12 @@ const babel = require('babel-core');
 const jsdom = require('jsdom');
 const path = require('path');
 
-
 before(function(done) {
   const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
     presets: ['es2015']
   });
 
-  //const src = path.resolve(__dirname, '..', 'index.js');
-
-  jsdom.env('<div></div>', [], {src: babelResult.code}, (err, window) => {
+  jsdom.env(path.resolve(__dirname, '..', 'index.html'), [], {src: babelResult.code}, (err, window) => {
     if (err) {
       return done(err);
     }
@@ -23,4 +20,4 @@ before(function(done) {
 
     return done();
   });
-}); 
+});
