b255fb8b222d683820f86f586a0a37df7ecb795e
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2017-01-18 14:03:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    return counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+
+}
+
+function createAnimal(animalType) {
+
+  return function(deadlyDevice) {
+    return {
+      animalType,
+      deadlyDevice
+    }
+  }
+}
+
+const sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+
+var sharkWithFrickinCannon = sharkCreator('Cannon')
